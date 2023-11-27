735ebaa645720129960a547b39398a8ef042dbd9
macalistervadim <macalistervadim@yandex.ru>
2023-11-27 16:19:51 +0600

    Create db

---
+++ application/database/models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import BigInteger, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column, DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+from config import SQLACLHEMY_URL
+
+engine = create_async_engine(SQLACLHEMY_URL, echo=True)
+async_session = async_sessionmaker(engine)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id = mapped_column(BigInteger)
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+
+    products = relationship('Product', back_populates='category')
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    description: Mapped[str] = mapped_column()
+    price: Mapped[int] = mapped_column()
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
+
+    category = relationship('Category', back_populates='products')
+
+
+async def async_main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
--- run.py
+++ run.py
@@ -2,16 +2,19 @@ import logging
 import sys
 import asyncio
 
-from application.handlers import router
 from aiogram import Bot, Dispatcher
 
+from application.handlers import router
+from application.database.models import async_main
 from config import TOKEN
 
-#test
 async def main():
+    await async_main()
+
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
+
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
